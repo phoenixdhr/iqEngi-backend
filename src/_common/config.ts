8cac3a9dc6584333dc7f97ff5bffe98c41477c5e
dreyes <phoenixdhr@gmail.com>
2024-04-04 18:24:46 -0500

    se configuro las variables de entorno

--- src/_common/config.ts
+++ src/_common/config.ts
@@ -13,6 +13,7 @@ export const configValidationSchema = Joi.object({
   API_KEY: Joi.string().required(),
   DATABASE_NAME: Joi.string().required(),
   DATABASE_PORT: Joi.number().required(),
+  PORT: Joi.number().required(),
 });
 
 export const port = process.env.PORT || 3000;
